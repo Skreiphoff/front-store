e380e82a44f5fafb407b03369295636af7c80182
a.skripnikov <a.skripnikov@b2b-center.ru>
2022-04-15 04:20:06 +0300

    Готовый проект

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -5,8 +5,8 @@ export const HOME_ROUTE = '/home';
 export const REGISTRATION_ROUTE = '/reg';
 export const LOGIN_ROUTE = '/login';
 export const PRODUCT_ROUTE = '/product';
-export const BACKEND_PATH = 'http://localhost:8080/api/v1/'
 
+export const BACKEND_PATH = 'http://localhost:8080/api/v1/'
 export const BACKEND_LOGIN_PATH = BACKEND_PATH + 'login'
 export const BACKEND_REGISTRATION_PATH = BACKEND_PATH + 'register'
 export const BACKEND_ME_PATH = BACKEND_PATH + 'me'
